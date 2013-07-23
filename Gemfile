1175b62a26dfd9286d0537ff148a4f4e3ab5a126
Joel Turnbull <joelbywan@gmail.com>
2013-07-23 19:47:46 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 4.0.0'
+ruby '2.0.0'
+
+gem 'pg'
+gem 'puma'
+gem 'jquery-rails'
+
+gem 'sass-rails',   '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+group :development, :test do
+  gem 'pry'
+  gem 'rspec-rails'
+  gem 'dotenv'
+  gem 'konacha'
+end
+
+group :test do
+#  gem 'poltergeist'
+  gem 'factory_girl_rails'
+  gem 'launchy'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommentService::Application.config.secret_token = '52f30b3eaeb0bcb2572b5703c84c92bda85e4a47109052def34ba0d2b95ce256473b8d513a0aa56412281621680500322b225d447abbc7a8abeff5de405fa231'
